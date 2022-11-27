6224a260acdf031980aa052bd8cb062981974faf
Maratah Njoroge <mnjoroge@plateaumed.com>
2022-11-27 22:01:46 +0300

    Fix routing

--- src/App.js
+++ src/App.js
@@ -1,28 +1,52 @@
 // import logo from './logo.svg';
-import React from 'react';
-import './App.css';
-import NavBar from './pages/NavBar';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import NavBar from "./pages/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home"
-import Footer from './pages/Footer';
-import Home2 from './pages/Home2';
-import LogIn from './pages/Authentication/LogIn';
+import Home from "./pages/home";
+import Footer from "./pages/Footer";
+import Home2 from "./pages/Home2";
+import LogIn from "./pages/Authentication/LogIn";
 import SenderFax from "./pages/Solutions/SenderFax";
-import SenderAI from './pages/Solutions/SenderAI';
+import SenderAI from "./pages/Solutions/SenderAI";
 function App() {
   return (
     <Router>
-    <NavBar />
-    <Home />
-    <Home2 />
-    <SenderAI />
-    <Routes>
+      <NavBar />
+      <Switch>
+        <Route exact path="/home">
+          <Home />
+          <Home2 />
+          <Footer />
+        </Route>
+        <Route exact path="/login">
+          <LogIn />
+        </Route>
+        <Route exact path="/SenderFax">
+          <SenderFax />
+        </Route>
+        <Route exact path="/SenderAI">
+          <SenderAI />
+        </Route>
+        {/* <Route exact path="/footer">
+          <Footer />
+        </Route> */}
+        {/* <Route path="/LogIn" exact element={<LogIn />} /> */}
+        {/* <Route path="/SenderFax" exact element={<SenderFax />} /> */}
+      </Switch>
+      {/* <NavBar /> */}
+      {/* <Home /> */}
+      {/* <Home2 /> */}
+      {/* <SenderAI /> */}
+      {/* <LogIn /> */}
+      {/* <SenderFax /> */}
+      {/* <Footer /> */}
+      {/* <Switch>
     <Route  path='/LogIn' exact element={<LogIn />} />
     <Route  path='/SenderFax' exact element={<SenderFax />} />
 
-    </Routes>
-    <Footer />
+    </Switch> */}
     </Router>
   );
 }
